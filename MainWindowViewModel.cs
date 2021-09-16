c6e2b97d57df143cbd0214d351861ba9fd8c22af
Artem Darkov <darkprothekun@gmail.com>
2021-09-16 15:26:42 +0300

    Initial commit

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MVVMTestApp
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string title = "Hello, MVVM!";
+        private string text;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChanged("Title"); }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; OnPropertyChanged("Text"); }
+        }
+
+        public class RelayCommand : ICommand
+        {
+            private Action<object> execute;
+            private Func<object, bool> canExecute;
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+            {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return this.canExecute == null || this.canExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                this.execute(parameter);
+            }
+        }
+
+        private RelayCommand changeTitle;
+        public RelayCommand ChangeTitle
+        {
+            get
+            {
+                return changeTitle ??
+                    (changeTitle = new RelayCommand(obj =>
+                    {
+                        Title = Text;
+                    }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
